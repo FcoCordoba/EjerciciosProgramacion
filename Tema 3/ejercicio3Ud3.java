a6a8b9dd63475c649cd53cf8da04fc706e0b4ba7
Francisco Cordoba <74825476+Frandaw21@users.noreply.github.com>
2020-11-21 16:19:01 +0100

    Add files via upload

---
+++ Tema 3/ejercicio3Ud3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ejercicio3Ud3{
+
+    public static void main(String[] args) {
+        
+
+        double a = 8;
+        double b = 3;
+        double c = 0;
+
+        c = a/b;
+        System.out.printf("la division es: \n" +c);
+        
+        System.out.printf("\n%.2f  ", c);
+        System.out.printf("\n%5.2f  ", c);
+        System.out.printf("\n%7.3f  ", c);
+        System.out.printf("\n%07.3f  ", c);
+        System.out.printf("\n%10.4f  ", c);
+        System.out.printf("\n%5.3f  ", c);
+        System.out.printf("\n%10.5f  ", c);
+        System.out.printf("\n%010.0f  ", c);
+
+    }
+
+
+}
